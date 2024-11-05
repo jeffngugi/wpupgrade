b9472d1a7ae923412644b998ecbfd17375b7c7cf
jeffngugi <ngugimwathi254@gmail.com>
2024-11-05 15:03:54 +0300

    ch:android build types

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Config from "react-native-config";
 
 const App = () => {
+  console.log('The config is', Config)
   return (
     <View style={{alignItems: 'center', flex: 1, justifyContent:'center'}}>
       <Text>Test react native config for android</Text>
