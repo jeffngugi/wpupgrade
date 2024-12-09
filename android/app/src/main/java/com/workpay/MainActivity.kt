28233eb6f8e0bad6062fb8b1bf30afbd20ab0d77
jeffngugi <ngugimwathi254@gmail.com>
2024-12-09 11:52:31 +0300

    install navigation packages

--- android/app/src/main/java/com/workpay/MainActivity.kt
+++ android/app/src/main/java/com/workpay/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.workpay.workpay
 import expo.modules.ReactActivityDelegateWrapper
-
+import android.os.Bundle;
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -8,11 +8,16 @@ import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
 
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
+  
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
   override fun getMainComponentName(): String = "Workpay"
+  
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
